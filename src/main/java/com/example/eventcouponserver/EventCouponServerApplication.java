19f4e7b2c7d892e94a37a9629fa10ac8dad648ed
kki4504 <rhkddlf9876@gmail.com>
2023-08-22 02:33:41 +0900

    init

---
+++ src/main/java/com/example/eventcouponserver/EventCouponServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventcouponserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCouponServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCouponServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventcouponserver/EventCouponServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventcouponserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCouponServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
