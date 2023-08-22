379306770459919e2be37a3e966c5dc9674a62fa
kki4504 <rhkddlf9876@gmail.com>
2023-08-23 02:33:57 +0900

    feat: event 상태 체크 후 close 로 변경하는 스케줄러

---
+++ src/main/java/com/example/eventcouponserver/service/EventScheduler.java
@@ -0,0 +1,32 @@
+package com.example.eventcouponserver.service;
+
+import com.example.eventcouponserver.entity.Event;
+import com.example.eventcouponserver.repository.EventRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public class EventScheduler {
+
+    private final EventRepository eventRepository;
+
+    public EventScheduler(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Scheduled(cron = "${event.schedule.cron}")
+    @Transactional
+    public void closingEventScheduled() {
+        List<Event> openEvent = eventRepository.findEventsByEventStatus();
+        LocalDateTime now = LocalDateTime.now();
+        for (Event event : openEvent) {
+            if (event.getClosingTime().isBefore(now)) {
+                event.closeEventAtClosingTime();
+            }
+        }
+    }
+}
