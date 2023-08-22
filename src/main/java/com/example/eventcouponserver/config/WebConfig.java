9c7dfd557c2f59f46244e0b2d6d020f39fa74b9a
kki4504 <rhkddlf9876@gmail.com>
2023-08-23 02:31:44 +0900

    feat: 로그인 argumentResolver

---
+++ src/main/java/com/example/eventcouponserver/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.eventcouponserver.config;
+
+import com.example.eventcouponserver.member.LoginMemberArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*") //모든 헤더 허용
+                .exposedHeaders(HttpHeaders.AUTHORIZATION) //클라이언트가 응답에 접근할 수 있는 헤더 추가
+                .allowCredentials(true) // 모든 인증 정보 허용
+                .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+}
